471c87230e69c32843af0a6a03d5f65a1f4e2de6
duamkr <duamkr@gmail.com>
2019-05-29 16:53:46 +0900

    Section1-3

---
+++ Part3/sec1-3.R
@@ -0,0 +1,17 @@
+setwd("d:/Workspace/R_Data_Analysis/Part3/data")
+getwd()
+
+print(1+2)
+1+2
+
+print('a')
+
+print(pi)
+print(pi, digits=3)     #digits = 소수점 자리 
+print(3,4)     #3,4 중 3만 출력됨
+print('a','b')     #에러가 발생(2개의문자 표시)
+cat(1,':','a','\n','2',':','b')     #cat을 활용, 여러개의 문자를 표시 가능
+3; 4; 5;     # ;로 여러개의 문자 표시
+1+2; 3*4; pi*5*5
+
+  
